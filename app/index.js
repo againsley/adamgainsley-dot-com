97b985e13fc63a18cf4049694ea0d6837dc228a1
Adam Gainsley <akronymn@gmail.com>
2016-09-26 22:33:01 -0400

    Add node supervisor. Begin building resume page.

--- app/index.js
+++ app/index.js
@@ -11,10 +11,36 @@ app.set('view engine', 'ejs');
 app.set('views',__dirname + '/pages');
 
 
-// index page 
+// home 
 app.get('/', function(req, res) {
     res.render('index/index');
 });
+
+// blog 
+app.get('/blog', function(req, res) {
+    res.render('blog/blog');
+});
+
+// portfolios
+app.get('/portfolios', function(req, res) {
+    res.render('portfolios/portfolios');
+});
+
+// resume
+app.get('/resume', function(req, res) {
+    res.render('resume/resume');
+});
+
+// about
+app.get('/about', function(req, res) {
+    res.render('about/about');
+});
+
+// contact
+app.get('/contact', function(req, res) {
+    res.render('contact/contact');
+});
+
 
 app.listen(port, (err) => {  
   if (err) {
